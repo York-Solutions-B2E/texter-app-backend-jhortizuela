f082cbf76e266d1baaf7d5dac1f22443bf66465c
jhortizu01 <jhortizuela21@gmail.com>
2025-01-13 10:54:29 -0700

    first commit

---
+++ src/test/java/com/example/texterApp/TexterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.texterApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
